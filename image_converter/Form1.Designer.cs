8952574c57d9a4325c97302ae1fb880c38940833
Ali Ahmadi <aliahmadi79sh@gmail.com>
2024-09-06 09:04:57 +0330

    Final Commit For Push

---
+++ image_converter/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace image_converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupPanel1 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.btnselect = new DevComponents.DotNetBar.ButtonX();
+            this.groupPanel2 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.radialMenu1 = new DevComponents.DotNetBar.RadialMenu();
+            this.Gif = new DevComponents.DotNetBar.RadialMenuItem();
+            this.Jpg = new DevComponents.DotNetBar.RadialMenuItem();
+            this.Png = new DevComponents.DotNetBar.RadialMenuItem();
+            this.Icon = new DevComponents.DotNetBar.RadialMenuItem();
+            this.groupPanel1.SuspendLayout();
+            this.groupPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupPanel1
+            // 
+            this.groupPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.groupPanel1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.groupPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.groupPanel1.Controls.Add(this.btnselect);
+            this.groupPanel1.Controls.Add(this.groupPanel2);
+            this.groupPanel1.DisabledBackColor = System.Drawing.Color.Empty;
+            this.groupPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupPanel1.Location = new System.Drawing.Point(0, 0);
+            this.groupPanel1.Name = "groupPanel1";
+            this.groupPanel1.Size = new System.Drawing.Size(516, 292);
+            // 
+            // 
+            // 
+            this.groupPanel1.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.groupPanel1.Style.BackColorGradientAngle = 90;
+            this.groupPanel1.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.groupPanel1.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderBottomWidth = 1;
+            this.groupPanel1.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.groupPanel1.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderLeftWidth = 1;
+            this.groupPanel1.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderRightWidth = 1;
+            this.groupPanel1.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderTopWidth = 1;
+            this.groupPanel1.Style.CornerDiameter = 4;
+            this.groupPanel1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.groupPanel1.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.groupPanel1.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.groupPanel1.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.groupPanel1.TabIndex = 0;
+            this.groupPanel1.Click += new System.EventHandler(this.groupPanel1_Click);
+            // 
+            // btnselect
+            // 
+            this.btnselect.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.btnselect.Checked = true;
+            this.btnselect.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.btnselect.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.btnselect.Location = new System.Drawing.Point(355, 9);
+            this.btnselect.Name = "btnselect";
+            this.btnselect.Shape = new DevComponents.DotNetBar.EllipticalShapeDescriptor();
+            this.btnselect.Size = new System.Drawing.Size(146, 54);
+            this.btnselect.Style = DevComponents.DotNetBar.eDotNetBarStyle.Office2013;
+            this.btnselect.TabIndex = 2;
+            this.btnselect.Text = "انتخاب تصویر";
+            this.btnselect.Click += new System.EventHandler(this.btnselect_Click);
+            // 
+            // groupPanel2
+            // 
+            this.groupPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.groupPanel2.CanvasColor = System.Drawing.SystemColors.Control;
+            this.groupPanel2.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.groupPanel2.Controls.Add(this.radialMenu1);
+            this.groupPanel2.DisabledBackColor = System.Drawing.Color.Empty;
+            this.groupPanel2.Location = new System.Drawing.Point(3, 3);
+            this.groupPanel2.Name = "groupPanel2";
+            this.groupPanel2.Size = new System.Drawing.Size(334, 280);
+            // 
+            // 
+            // 
+            this.groupPanel2.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.groupPanel2.Style.BackColorGradientAngle = 90;
+            this.groupPanel2.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.groupPanel2.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel2.Style.BorderBottomWidth = 1;
+            this.groupPanel2.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.groupPanel2.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel2.Style.BorderLeftWidth = 1;
+            this.groupPanel2.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel2.Style.BorderRightWidth = 1;
+            this.groupPanel2.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel2.Style.BorderTopWidth = 1;
+            this.groupPanel2.Style.CornerDiameter = 4;
+            this.groupPanel2.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.groupPanel2.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.groupPanel2.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.groupPanel2.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.groupPanel2.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.groupPanel2.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.groupPanel2.TabIndex = 1;
+            // 
+            // radialMenu1
+            // 
+            // 
+            // 
+            // 
+            this.radialMenu1.Colors.RadialMenuItemDisabledForeground = System.Drawing.Color.Empty;
+            this.radialMenu1.Items.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.Gif,
+            this.Jpg,
+            this.Png,
+            this.Icon});
+            this.radialMenu1.Location = new System.Drawing.Point(143, 110);
+            this.radialMenu1.Name = "radialMenu1";
+            this.radialMenu1.Size = new System.Drawing.Size(28, 28);
+            this.radialMenu1.Symbol = "";
+            this.radialMenu1.SymbolSize = 13F;
+            this.radialMenu1.TabIndex = 0;
+            this.radialMenu1.Text = "radialMenu1";
+            // 
+            // Gif
+            // 
+            this.Gif.Name = "Gif";
+            this.Gif.Text = "Gif";
+            this.Gif.Click += new System.EventHandler(this.Gif_Click);
+            // 
+            // Jpg
+            // 
+            this.Jpg.Name = "Jpg";
+            this.Jpg.Text = "Jpg";
+            this.Jpg.Click += new System.EventHandler(this.Jpg_Click);
+            // 
+            // Png
+            // 
+            this.Png.Name = "Png";
+            this.Png.Text = "Png";
+            this.Png.Click += new System.EventHandler(this.Png_Click);
+            // 
+            // Icon
+            // 
+            this.Icon.Name = "Icon";
+            this.Icon.Text = ".ico";
+            this.Icon.Click += new System.EventHandler(this.Icon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 292);
+            this.Controls.Add(this.groupPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Image converter";
+            this.groupPanel1.ResumeLayout(false);
+            this.groupPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.Controls.GroupPanel groupPanel1;
+        private DevComponents.DotNetBar.ButtonX btnselect;
+        private DevComponents.DotNetBar.Controls.GroupPanel groupPanel2;
+        private DevComponents.DotNetBar.RadialMenu radialMenu1;
+        private DevComponents.DotNetBar.RadialMenuItem Gif;
+        private DevComponents.DotNetBar.RadialMenuItem Jpg;
+        private DevComponents.DotNetBar.RadialMenuItem Png;
+        private DevComponents.DotNetBar.RadialMenuItem Icon;
+    }
+}
+
---
+++ image_converter/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace image_converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupPanel1_Click(object sender, EventArgs e)
+        {
+
+        }
+        Image image;
+        private void btnselect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            op.Filter = "Image Files (.jpg)|*.jpg|(.png)|*.png|(.gif)|*.gif|(.ico)|*.ico";
+            if (op.ShowDialog() == DialogResult.OK)
+            {
+                image = Image.FromFile(op.FileName);
+            }
+        }
+        SaveFileDialog sv = new SaveFileDialog();
+        private void Gif_Click(object sender, EventArgs e)
+        {
+            sv.ShowDialog();
+            image.Save(sv.FileName + ".gif", System.Drawing.Imaging.ImageFormat.Gif);
+        }
+        private void Jpg_Click(object sender, EventArgs e)
+        {
+            sv.ShowDialog();
+            image.Save(sv.FileName + ".Jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+        }
+        private void Png_Click(object sender, EventArgs e)
+        {
+            sv.ShowDialog();
+            image.Save(sv.FileName + ".Png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+        private void Icon_Click(object sender, EventArgs e)
+        {
+            sv.ShowDialog();
+            image.Save(sv.FileName + ".ico", System.Drawing.Imaging.ImageFormat.Icon);
+        }
+    }
+}
